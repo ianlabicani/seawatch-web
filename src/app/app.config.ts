33e26f6c6785bc1864a278955a14ca21f656cfa8
ianlabicani <labicaniian@gmail.com>
2024-12-25 00:20:56 +0800

    add Firebase configuration and emulator setup to app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,49 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  isDevMode,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import {
+  connectFirestoreEmulator,
+  getFirestore,
+  provideFirestore,
+} from '@angular/fire/firestore';
+import { connectAuthEmulator, getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: 'AIzaSyCEstX6G_E9ijV32zbMcLpuBgUqyRlgito',
+        authDomain: 'seawatch-web.firebaseapp.com',
+        projectId: 'seawatch-web',
+        storageBucket: 'seawatch-web.firebasestorage.app',
+        messagingSenderId: '772755499356',
+        appId: '1:772755499356:web:7decfd4434f5bd1b3db999',
+        measurementId: 'G-GEEQE94NK1',
+      })
+    ),
+    provideFirestore(() => {
+      const firestore = getFirestore();
+      if (isDevMode()) {
+        connectFirestoreEmulator(firestore, 'localhost', 8080);
+      }
+      return firestore;
+    }),
+    provideAuth(() => {
+      const auth = getAuth();
+      if (isDevMode()) {
+        connectAuthEmulator(auth, 'http://localhost:9099', {
+          disableWarnings: true,
+        });
+      }
+      return auth;
+    }),
+  ],
 };
