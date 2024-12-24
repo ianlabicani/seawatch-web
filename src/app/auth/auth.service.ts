49eef620b497bb6aa584b96c2a477f5a784db965
ianlabicani <labicaniian@gmail.com>
2024-12-25 01:34:07 +0800

    add authentication service with login and logout functionality

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,50 @@
+import { inject, Injectable } from '@angular/core';
+import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { getDoc, doc, Firestore } from '@angular/fire/firestore';
+
+export interface IUserAuth {
+  uid: string;
+  email: string;
+  role: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private auth = inject(Auth);
+  private firestore = inject(Firestore);
+
+  constructor() {}
+
+  async login(email: string, password: string) {
+    const { user } = await signInWithEmailAndPassword(
+      this.auth,
+      email,
+      password
+    );
+
+    const snapshot = await getDoc(doc(this.firestore, 'users', user.uid));
+    this.setUserAuth({ ...snapshot.data() } as IUserAuth);
+  }
+
+  async logout() {
+    await this.auth.signOut();
+    this.removeUserAuth();
+  }
+
+  private setUserAuth(user: IUserAuth) {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  getUserAuth() {
+    const user = localStorage.getItem('user');
+    if (!user) return null;
+
+    return JSON.parse(user);
+  }
+
+  private removeUserAuth() {
+    localStorage.removeItem('user');
+  }
+}
