034e0cd1f7dbc64ca6a856b92c7b6522a8f3f83a
ianlabicani <labicaniian@gmail.com>
2024-12-25 02:20:20 +0800

    refactor: remove unused Bootstrap import from main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-// @ts-ignore
-import bootstrap from 'bootstrap';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
