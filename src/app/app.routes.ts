2e5748f64fe0a8eb2e34d3dc2930e8ac8f67a9ab
ianlabicani <labicaniian@gmail.com>
2024-12-25 00:27:30 +0800

    add routing for welcome component and update default redirect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,26 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'welcome',
+    pathMatch: 'full',
+  },
+  {
+    path: 'welcome',
+    loadComponent: () =>
+      import('./welcome/welcome.component').then((m) => m.WelcomeComponent),
+  },
+  {
     path: 'auth',
     loadComponent: () =>
       import('./auth/auth.component').then((m) => m.AuthComponent),
     loadChildren: () => import('./auth/auth.routes').then((m) => m.routes),
   },
   {
-    path: '',
-    redirectTo: 'auth',
-    pathMatch: 'full',
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./coast-guard/coast-guard.component').then(
+        (m) => m.CoastGuardComponent
+      ),
   },
 ];
