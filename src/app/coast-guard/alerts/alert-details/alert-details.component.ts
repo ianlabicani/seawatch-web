0675a9521233a662f3467e3ba96f7efd86804b1d
ianlabicani <labicaniian@gmail.com>
2025-01-05 14:41:20 +0800

    feat: enhance alert details component with user information and report handling

--- src/app/coast-guard/alerts/alert-details/alert-details.component.ts
+++ src/app/coast-guard/alerts/alert-details/alert-details.component.ts
@@ -17,7 +17,7 @@ import {
   updateDoc,
 } from '@angular/fire/firestore';
 
-import { IAlert, ITracking } from '../../../shared/models';
+import { IAlert, IReport, ITracking } from '../../../shared/models';
 import { DatePipe, JsonPipe } from '@angular/common';
 import { Router } from '@angular/router';
 import { MapComponent } from '../../../shared/components/map/map.component';
@@ -33,7 +33,6 @@ import { IUserAuth } from '../../../auth/auth.service';
     MapComponent,
     ReactiveFormsModule,
     ReportsDetailsComponent,
-    JsonPipe,
   ],
   templateUrl: './alert-details.component.html',
   styleUrl: './alert-details.component.scss',
@@ -47,6 +46,7 @@ export class AlertDetailsComponent implements OnInit {
 
   id = input.required<string>();
   alertSig = signal<IAlert | null>(null);
+  reportSig = signal<IReport | null>(null);
   trackingSig = signal<ITracking | null>(null);
   userSig = signal<IUserAuth | null>(null);
   destroyRef = inject(DestroyRef);
@@ -69,70 +69,86 @@ export class AlertDetailsComponent implements OnInit {
   });
 
   isSubmitting = signal(false);
-
-  async getAlertWithUserAndTracking(alertId: string) {
-    // Reference for alert
-    const alertRef = doc(this.firestore, `alerts/${alertId}`);
-    const alertDoc = await getDoc(alertRef);
-    const alert = { ...alertDoc.data(), id: alertDoc.id } as IAlert;
-
-    // Fetch tracking and user data concurrently
-    const [trackingDoc, userDoc] = await Promise.all([
-      getDoc(doc(this.firestore, `trackings/${alert.trackingId}`)), // Fetch tracking
-      getDoc(doc(this.firestore, `users/${alert.uid}`)), // Fetch user directly
-    ]);
-
-    // Extract data
-    const tracking = { ...trackingDoc.data(), id: trackingDoc.id } as ITracking;
-    const user = { ...userDoc.data(), id: userDoc.id } as IUserAuth;
-
-    return Promise.resolve({ alert, tracking, user });
-  }
+  isLoading = signal(true);
 
   ngOnInit() {
     this.formGroup.controls.alertId.setValue(this.id());
 
-    from(this.getAlertWithUserAndTracking(this.id())).subscribe(
-      ({ alert, tracking, user }) => {
-        this.alertSig.set(alert);
-        this.trackingSig.set(tracking);
-        this.userSig.set(user);
+    from(this.getAlert(this.id())).subscribe(async (alert) => {
+      this.alertSig.set(alert);
+      const [report, tracking, user] = await Promise.all([
+        this.getReport(alert.reportId ?? undefined),
+        this.getTracking(alert.trackingId),
+        this.getUser(alert.uid),
+      ]);
 
-        const alertPoint = alert.geoPoint;
-        const startPoint = tracking.tracks[0];
+      this.reportSig.set(report);
+      this.trackingSig.set(tracking);
+      this.userSig.set(user);
 
-        // alert point
-        this.mapRefSig()
-          .addAlertMarker(alert, user)
-          .addTo(this.mapRefSig().map);
+      this.isLoading.set(false);
 
-        // startpoint
-        this.mapRefSig()
-          .addStartPointMarker(tracking)
-          .addTo(this.mapRefSig().map);
+      // alert point
+      this.mapRefSig().addAlertMarker(alert, user).addTo(this.mapRefSig().map);
 
-        // polyline
-        const trackPoints: { latitude: number; longitude: number }[] =
-          tracking.tracks.map((track) => ({
-            latitude: track.latitude,
-            longitude: track.longitude,
-          }));
-        this.mapRefSig()
-          .addPolyLine(trackPoints, {
-            color: 'red',
-            weight: 3,
-            opacity: 0.7,
-            lineJoin: 'round',
-          })
-          .addTo(this.mapRefSig().map);
+      // startpoint
+      this.mapRefSig()
+        .addStartPointMarker(tracking)
+        .addTo(this.mapRefSig().map);
 
-        // endpoint
-        const endPoint = tracking.tracks[tracking.tracks.length - 1];
-        this.mapRefSig()
-          .addEndPointMarker(tracking)
-          .addTo(this.mapRefSig().map);
-      }
-    );
+      // polyline
+      const trackPoints: { latitude: number; longitude: number }[] =
+        tracking.tracks.map((track) => ({
+          latitude: track.latitude,
+          longitude: track.longitude,
+        }));
+      this.mapRefSig()
+        .addPolyLine(trackPoints, {
+          color: 'red',
+          weight: 3,
+          opacity: 0.7,
+          lineJoin: 'round',
+        })
+        .addTo(this.mapRefSig().map);
+
+      // endpoint
+      this.mapRefSig().addEndPointMarker(tracking).addTo(this.mapRefSig().map);
+    });
+  }
+
+  async getAlert(alertId: string) {
+    const alertRef = doc(this.firestore, `alerts/${alertId}`);
+    const alertDoc = await getDoc(alertRef);
+    return { ...alertDoc.data(), id: alertDoc.id } as IAlert;
+  }
+
+  async getReport(reportId?: string) {
+    if (!reportId) {
+      return null;
+    }
+
+    const reportRef = doc(this.firestore, `reports/${reportId}`);
+    const reportDoc = await getDoc(reportRef);
+
+    if (!reportDoc.exists()) {
+      return null;
+    }
+
+    const report = { ...reportDoc.data(), id: reportDoc.id } as IReport;
+
+    return report;
+  }
+
+  async getTracking(trackingId: string) {
+    const trackingRef = doc(this.firestore, `trackings/${trackingId}`);
+    const trackingDoc = await getDoc(trackingRef);
+    return { ...trackingDoc.data(), id: trackingDoc.id } as ITracking;
+  }
+
+  async getUser(userId: string) {
+    const userRef = doc(this.firestore, `users/${userId}`);
+    const userDoc = await getDoc(userRef);
+    return { ...userDoc.data(), id: userDoc.id } as IUserAuth;
   }
 
   async resolveAlert() {
@@ -158,6 +174,12 @@ export class AlertDetailsComponent implements OnInit {
       await updateDoc(doc(this.firestore, `alerts/${this.id()}`), {
         isResolved: true,
         reportId: docRef.id,
+      });
+      this.isSubmitting.set(false);
+      this.isCreateReport.set(false);
+      this.reportSig.set({
+        ...data,
+        id: docRef.id,
       });
     } catch (error) {
       console.error('Error creating report:', error);
